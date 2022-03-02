2213fbacc88cd753707a5cadf1efaa7b5a3cc71b
BreakinParallels <93020898+BreakinParallels@users.noreply.github.com>
2022-03-02 13:45:17 -0500

    add boolean submission assignment

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //examples of &&(AND) operator
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+            //Console.ReadLine();
+
+
+            //examples of ||(OR) operator
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+
+
+            //examples of ==(EQUALS) operator            
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+            //Console.ReadLine();
+
+
+            //examples of !=(DOES NOT EQUAL) operator
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+            //Console.ReadLine();
+
+
+            //examples of ^(XOR) operator
+            //Console.WriteLine(true ^ true);
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(false ^ false);
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ Boolean_Logic_Submission_Assignment/Boolean_Logic_Submission_Assignment/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Boolean_Logic_Submission_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //prints title
+            Console.WriteLine("Insurance Questionaire");
+
+            //asks question to user
+            Console.WriteLine("What is your age?");
+            //states variable and what type 
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            //asks question to user
+            Console.WriteLine("Have you ever had a DUI? true or false?");
+            
+            bool hasDUI = Convert.ToBoolean(Console.ReadLine());
+
+            //asks question to user           
+            Console.WriteLine("How many tickets have you had?");
+            //states variable and what type
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            //gives program boolean to determine if qualified or not based on user's answers
+            bool qualified;
+            if(age > 15 && hasDUI == false && tickets <= 3)
+            {
+                qualified = true;
+
+            }
+            else
+            {
+                qualified = false;
+            }
+            //prints quotated to user
+            Console.WriteLine("Qualified?");
+            //prints answer based on answers using boolean logic above
+            Console.WriteLine(qualified);
+            
+            Console.ReadLine();
+        }
+    }
+}
