8be599f1e72120eb352c4bb970eb0b1b118cc2bb
BreakinParallels <93020898+BreakinParallels@users.noreply.github.com>
2022-03-02 18:26:29 -0500

    BranchingSubmission

---
+++ BranchingSubmissionAssignment/BranchingSubmissionAssignment/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //displays intro to user
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            //asks user to enter data, data type is stated to program(integer)
+            Console.WriteLine("Please enter the package weight:");
+            int wt = Convert.ToInt32(Console.ReadLine());
+
+            //Branch statement is created to make sure certain criteria is met so program can determine next action using ("if" and "else" statements)
+            if (wt > 50)
+            {
+                //if criteria is not met, the message below is printed to user, also ending the program
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            }
+            //if criteria is met, program will continue asking for more criteria to be met
+            else
+            {
+                //asks user to enter data, data type is stated to program(integer)
+                Console.WriteLine("Package width?");
+                int width = Convert.ToInt32(Console.ReadLine());
+
+                //asks user to enter data, data type is stated to program(integer)
+                Console.WriteLine("Package height?");
+                int ht = Convert.ToInt32(Console.ReadLine());
+
+                //asks user to enter data, data type is stated to program(integer)
+                Console.WriteLine("Package length?");
+                int leng = Convert.ToInt32(Console.ReadLine());
+
+                //program takes data, adds it up to define variable given: sumDimensions
+                int sumDimensions = (width + ht + leng); 
+                                             
+                //takes variable(sumDimensions), compares it in branching statement
+                if (sumDimensions > 50)
+                    {
+                    //if criteria is not met, the message below is printed to user, also ending the program    
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                    }
+                else
+                {                    
+                    //if criteria is met, program uses equation stipulated below
+                    decimal shippingCost = leng * width * ht * wt / 100;
+                    //the total of equation that is variable(shippingCost), is added to the end of WriteLine statement giving the user their total
+                    Console.WriteLine("Your estimated total for shipping this package is:$" + shippingCost);
+                 
+                }
+
+            }
+
+           
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BranchingTutorialExamples/BranchingTutorialExamples/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingTutorialExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //TERNERY EXAMPLE:
+            Console.WriteLine("What is your favorite number?");
+            int favNum = Convert.ToInt32(Console.ReadLine());
+
+            string result = favNum == 12 ? "You have an awesome favorite number." : "You do not have an awesome favorite number.";
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+
+            //ELSE/ ELSE IF operator example:
+            //int roomTemperature = 70;
+
+            //Console.WriteLine("Hi, what is your name?");
+            //string name = Console.ReadLine();
+
+            //Console.WriteLine("Hi, " + name + ", what is the temperature where you are?");
+            //int currentTemperature = Convert.ToInt32(Console.ReadLine());
+
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("It is exactly room temperture.");
+            //}
+            //else if (currentTemperature > roomTemperature)
+            //{
+            //    Console.WriteLine("It is warmer than room temperature.");
+            //}
+            //else if (roomTemperature > currentTemperature)
+            //{
+            //    Console.WriteLine("It is colder than room temperature.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Uhhhh....something went wrong.");
+            //}
+
+            //Console.ReadLine();
+
+
+            //int currentTemperature = 80;
+            //int roomTemperature = 70;
+
+            ////examples of Ternery Operator in use
+            //string comparisonResult = currentTemperature == roomTemperature ? "It is room temp." : "It is not room temp.";
+
+            //Console.WriteLine(comparisonResult);
+            //Console.ReadLine();
+            
+            //example of ELSE/ELSE IF STATEMENTS
+            //if (currentTemperature == roomTemperature) 
+            //{
+            //    Console.WriteLine("It is exactly room temperature.");
+            //}
+            //else if (currentTemperature > roomTemperature)
+            //{
+            //    Console.WriteLine("It is warmer than room temperature.");
+            //}
+            //else if (roomTemperature > currentTemperature)
+            //{
+            //    Console.WriteLine("Room temperature is warmer than current temperature.");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("It is not exactly room temperature.");
+            //}
+            //Console.WriteLine();
+        }
+    }
+}
