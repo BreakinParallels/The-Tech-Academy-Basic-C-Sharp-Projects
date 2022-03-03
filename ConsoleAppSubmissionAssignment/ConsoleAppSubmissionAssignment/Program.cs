565cb45a83de119fafcd8c6bc8a1d50ce2c456ab
BreakinParallels <93020898+BreakinParallels@users.noreply.github.com>
2022-03-03 18:37:57 -0500

    add fixes

--- ConsoleAppSubmissionAssignment/ConsoleAppSubmissionAssignment/Program.cs
+++ ConsoleAppSubmissionAssignment/ConsoleAppSubmissionAssignment/Program.cs
@@ -13,7 +13,7 @@ namespace ConsoleAppSubmissionAssignment
             //creates a boolean logic statement declaring what the correct "answer"/string is
             bool isGuessed = nflTeam == "Rams";
 
-            //begins our "do statement" 
+            //begins our "do statement"
             do
             {
                 //within our "do statement" our "switch statement" is introduced giving different responses
@@ -39,17 +39,35 @@ namespace ConsoleAppSubmissionAssignment
                         Console.WriteLine("You guessed the Rams. That is the correct team!");
                         Console.ReadLine();
                         break;
-                    default:
-                        Console.WriteLine("You are wrong. Select another team.");
-                        Console.WriteLine("Guess another team?");
-                        nflTeam = Console.ReadLine();
-                        break;
+
                 }
             }
-            //our "while statement" is given 
             while (!isGuessed);
 
-            Console.Read();
+            //declares our data type and variable for our "while loop"
+            int counter = 0;
+            while(counter < 10)
+            {
+                //tells our program to display numbers from 0 until value reaches 10, which it will stop, essentially ending "while loop"
+                Console.WriteLine(counter);
+                counter++;
+            }
+
+            Console.ReadLine();
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+     
+            
         }
     }
 }
---
+++ StringFunctionExamples/StringFunctionExamples/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctionExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Shannnon";
+            //Console.WriteLine(name);
+
+            //string quote = "The man said, \"Hello\", Shannon.";
+            //Console.WriteLine(quote);
+
+            //string quote2 = "The man said, \"Hello\", Shannon. \nHello on a new line. \n \t Hello on a new tab.";
+            //Console.WriteLine(quote2);
+
+            //string fileName = "C:\\Users\\Shannon";
+            //Console.WriteLine(fileName);
+
+            //string fileName2 = @"C:\Users\Shannon";
+            //Console.WriteLine(fileName2);
+
+            //bool trueOrFalse = name.Contains("S");
+            //Console.WriteLine(trueOrFalse);
+
+            //bool trueOrFalse2 = name.Contains("s");
+            //char has to be exact, even the capitalization
+            //Console.WriteLine(trueOrFalse2);
+
+            //trueOrFalse = name.EndsWith("t");
+            //Console.WriteLine(trueOrFalse);
+
+            //int length = name.Length;
+            //Console.WriteLine(length);
+
+            //name = name.ToUpper();
+            //Console.WriteLine(name);
+
+            //string fname = "Shannon";
+            //fname = "Hank";
+            //Console.WriteLine(fname);
+            //Console.ReadLine();
+
+            //StringBuilder sb = new StringBuilder();
+            //sb.Append("My name is Shannon");
+            //Console.WriteLine(sb);
+            //Console.ReadLine();
+
+
+            
+            
+            
+            
+            
+            
+
+            
+
+            
+            
+            
+            
+
+        }
+    }
+}
