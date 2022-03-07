86db92110fc2938d81189d95e7dcffe19dcd8b3c
BreakinParallels <93020898+BreakinParallels@users.noreply.github.com>
2022-03-07 17:28:52 -0500

    ConsoleApp Assignments
    Updating up to Assignment 5

---
+++ ConsoleApp6PartAssignment/ConsoleApp6PartAssignment/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp6PartAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //setting the data type and variable with corresponding index in array
+            string[] colors = { "red", "blue", "black", "yellow", "green", "purple" };
+            
+            //program will ask user question, their choice will be converted to an integer which will correspond to position in index
+            Console.WriteLine("Please choose a color: red, blue, black, yellow, green, or purple.");
+            string colorChoice = Console.ReadLine();
+
+            // Will add user input to string array
+            for (int oldArray = 0; oldArray < colors.Length; oldArray++)
+            {
+                colors[oldArray] = colors[oldArray] + colorChoice;
+            }
+
+            //Will display new string array with user input
+            for (int updatedArray = 0; updatedArray < colors.Length; updatedArray++)
+            {
+                Console.WriteLine("Your new color string:" + colors[updatedArray]);
+
+            }
+
+            bool condition = true;
+
+            /* Infinite loop code
+            while(condition)
+            {
+                Console.WriteLine("This is a broken loop.");
+            }
+            */
+
+            //Fixed infite loop code
+            while (condition)
+            {
+                Console.WriteLine("This is a fixed inifite loop.");
+                condition = false;
+            }
+
+            //A loop where the comparison that’s used to determine whether to continue iterating the loop is a “<” operator.
+            for (int oldArray = 0; oldArray < colors.Length; oldArray++)
+            {
+                colors[oldArray] = colors[oldArray] + colorChoice;
+            }
+
+            //A loop where the comparison that’s used to determine whether to continue iterating the loop is a “<=” operator.
+            for (int oldArray = 0; oldArray <= 5; oldArray++)
+            {
+                colors[oldArray] = colors[oldArray] + colorChoice;
+            }
+
+            //test display for the above two loops
+            /*for (int updatedArray = 0; updatedArray < colors.Length; updatedArray++)
+            {
+                Console.WriteLine("Your new color string:" + colors[updatedArray]);
+
+            }
+            */
+
+            //A list of strings where each item in the list is unique
+            List<string> names = new List<string>() { "Bill", "Bob", "Becky", "Barb" };
+            //Ask the user to input text to search for in the list.
+            Console.WriteLine("Please write a name: Bill, Bob, Becky, Barb");
+            string myName = Console.ReadLine();
+
+            //A loop that iterates through the list and then displays the index of the list that contains matching text on the screen.           
+            int index = 0;
+            foreach (string name in names)
+            {
+                if (name == myName)
+                {
+                    Console.WriteLine(index);
+                    //Add code to the loop that stops it from executing once a match has been found.
+                    break;
+                }
+                index++;
+
+                //Add code to the loop to check if the user put in text that isn't on the list and, if they did, tells the user their input is not on the list.
+                if (index > 3)
+                {   
+                    Console.WriteLine("Name is not on the list.");
+                }
+             }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ IterationTutorialExamples/IterationTutorialExamples/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //int[] testScores = { 98, 99, 85, 70, 82, 34, 91, 90, 94 };
+
+        //for (int i = 0; i < testScores.Length; i++)
+        //{
+        //if (testScores[i] > 85)
+        //    {
+        //        Console.WriteLine("Passing test score: " + testScores[i]);
+        //    }
+
+        //}
+        //Console.ReadLine();
+
+
+        //string[] names = { "Jesse", "Erik", "Daniel", "Adam" };
+
+        //for (int j = 0; j < names.Length; j++)
+        //{
+        //    //if (names[j] == "Jesse")
+
+        //    {
+        //        Console.WriteLine(names[j]);
+        //    }
+        //    Console.ReadLine();
+
+
+        //List<string> names = new List<string>() { "Jesse", "Erik", "Adam", "Daniel" };
+
+        //foreach (string name in names)
+        //{
+        //    if (name == "Jesse")
+        //    {
+        //        Console.WriteLine(name);
+        //    }
+        //}
+        //Console.ReadLine();
+
+
+        List<int> testScores = new List<int>() { 98, 99, 12, 74, 23, 99 };
+        List<int> passingScores = new List<int>();
+
+        foreach (int score in testScores)
+        {
+            if (score > 85)
+            {
+                passingScores.Add(score);
+            }
+        }
+        Console.WriteLine(passingScores.Count);
+        Console.ReadLine();
+
+
+        }
+
+
+
+    }
+    
+
