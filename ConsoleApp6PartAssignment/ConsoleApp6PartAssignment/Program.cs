9ec20e0e52c1a89e2c80ab442cdc79585709b63a
BreakinParallels <93020898+BreakinParallels@users.noreply.github.com>
2022-03-07 21:05:45 -0500

    ConsoleApp6partAssignment
    added criteria

--- ConsoleApp6PartAssignment/ConsoleApp6PartAssignment/Program.cs
+++ ConsoleApp6PartAssignment/ConsoleApp6PartAssignment/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp6PartAssignment
         {
             //setting the data type and variable with corresponding index in array
             string[] colors = { "red", "blue", "black", "yellow", "green", "purple" };
-            
+
             //program will ask user question, their choice will be converted to an integer which will correspond to position in index
             Console.WriteLine("Please choose a color: red, blue, black, yellow, green, or purple.");
             string colorChoice = Console.ReadLine();
@@ -83,13 +83,71 @@ namespace ConsoleApp6PartAssignment
 
                 //Add code to the loop to check if the user put in text that isn't on the list and, if they did, tells the user their input is not on the list.
                 if (index > 3)
-                {   
+                {
                     Console.WriteLine("Name is not on the list.");
                 }
-             }
+            }
 
+            //A list of strings that has at least two identical strings in the list.
+            List<string> doubleNames = new List<string>() { "Bill", "Bob", "Becky", "Barb", "Bob" };
+            //Ask the user to input text to search for in the list.
+            Console.WriteLine("Please write a name: Bill, Bob, Becky, Barb");
+            string myNameDouble = Console.ReadLine();
+
+            //Create a loop that iterates through the list and then displays the indices of the items matching the user - selected text.
+
+            int index2 = 0;
+            foreach (string name in doubleNames)
+            {
+                if (name == myNameDouble)
+                {
+                    Console.WriteLine(index2);
+                }
+                else
+                {
+                    //Add code to the loop to check if the user put in text that isn't on the list and, if they did, tells the user their input is not on the list.
+                    if (index2 >= 4)
+                    {
+                        Console.WriteLine("Name is not on the list.");
+                        break;
+                    }
+                }
+                index2++;
+
+            }
+
+
+
+            
+            //Created a list of strings that has at least two identical strings in the list
+            List<int> examScores = new List<int>() { 98, 99, 12, 74, 23, 99, 98, 45, 74, 82, 77, 98, 85 };
+            List<int> sameScores = new List<int>();
+            //Create a foreach loop that evaluates each item in the list 
+            foreach (int score in examScores)
+            {
+                Console.WriteLine(score);
+            //displays a message showing the string and whether or not it has already appeared in the list.
+                if (sameScores.Contains(score))
+                {
+                    Console.WriteLine("This number has repeated.");
+                }
+                else
+                {
+                    Console.WriteLine("This number has NOT repeated");
+                }
+                sameScores.Add(score);
+            }
+            
             Console.ReadLine();
+        
+
+
+
+
+        }
+
+            
 
         }
     }
-}
+
