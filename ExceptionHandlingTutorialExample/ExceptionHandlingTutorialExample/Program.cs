571b037880e998b2519012047ab95e82dd1f408b
BreakinParallels <93020898+BreakinParallels@users.noreply.github.com>
2022-03-07 22:32:40 -0500

    strings and integers assignment
    add

---
+++ ExceptionHandlingTutorialExample/ExceptionHandlingTutorialExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ExceptionHandlingTutorialExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Pick a number.");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a second number.");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Dividing the two...");
+                int numberThree = numberOne / numberTwo;
+                Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+                Console.ReadLine();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type a whole number.");
+                return;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't divide by zero");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
