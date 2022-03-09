7c619af1a48d587b51b35918dd8cea6a01cb2a24
BreakinParallels <93020898+BreakinParallels@users.noreply.github.com>
2022-03-09 14:05:46 -0500

    diffMethodTypes program

---
+++ DiffMethClassSub/DiffMethClassSub/myMethod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DifferentMethodTypes
+{
+    public class myMethod
+    {
+
+        public void myMethodOp(int userNum)
+        {
+            //displays message to input integer of their choice and program will perform function on,
+            //which will display result at the end of the message
+            Console.WriteLine("Your number divided by two is: " + (userNum / 2));
+        }
+
+        public static int myMethodOp(int userNum, int myNum)
+        {
+            //displays how newNum is tabulated and will return that result to main program
+            int newNum = userNum + myNum;
+            return newNum;
+        }
+
+
+    }
+}
