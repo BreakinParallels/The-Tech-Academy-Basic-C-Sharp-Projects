5fdabc185b8cd3e8397c6549955103e97e91ccb4
BreakinParallels <93020898+BreakinParallels@users.noreply.github.com>
2022-03-08 20:59:32 -0500

    add methodSub Assignment

---
+++ MethodSubAssignment/MethodSubAssignment/MyMath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodSubAssignment
+{
+    class myMath
+    {
+        //sets parameter stating AddInts will be user's input for userNum and a default value of 0 if user does not give input for 2nd int
+        public static int AddInts(int userNum, int userNum2 = 0)
+        {
+            //creates variable from user's input for userNum + userNum2
+            int newUserNum = userNum + userNum2;
+            //returns value back to main program
+            return newUserNum;
+        }
+    }
+}
---
+++ MethodSubAssignment/MethodSubAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MethodSubAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //displays message asking for user input, takes input, converts it into an integer, assigns variable to integer
+            Console.WriteLine("Please input an integer: ");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            //asks user for 2nd number, lets them know they need not enter anything for the second number
+            //converts user's input into an integer
+            Console.WriteLine("Please input a second integer (optional): ");
+            bool userNum2IsValid = int.TryParse(Console.ReadLine(), out int userNum2);
+            //gives parameter based off user's input and what to do if user decides not to enter second integer
+            int result = 0;
+            if (userNum2IsValid)
+            {
+                result = myMath.AddInts(userNum, userNum2);
+            }
+            else
+            {
+                result = myMath.AddInts(userNum);
+            }
+
+            //displays message and adds user's result based on user's and parameters given above
+            Console.WriteLine("Your result is: " + result);
+            Console.ReadLine();
+        }
+    }
+}
