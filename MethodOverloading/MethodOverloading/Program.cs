b39b38ca9c2b98fd6b60300c4bbcb60b94523681
BreakinParallels <93020898+BreakinParallels@users.noreply.github.com>
2022-03-08 19:14:02 -0500

    add methodOverload Assignment

---
+++ MethodOverloading/MethodOverloading/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //asks user to input number to apply function to and converts input to an integer
+            Console.WriteLine("Please input an integer to operate on: ");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            //sets variable for function result applied to integer given by user
+            int newNum = mathFunction.myOverload(userNum);
+            // returns message with result of function 
+            Console.WriteLine("Your integer + 10 is: " + newNum);
+            Console.ReadLine();
+            //asks user to input number to apply function to and converts input to a float
+            Console.WriteLine("Please input a decimal to operate on: ");
+            float userNumFloat = float.Parse(Console.ReadLine());
+            //sets variable for function result applied to float/decimal number given by user
+            int newNumFloatInt = mathFunction.myOverload(userNumFloat);
+            // returns message with result of function
+            Console.WriteLine("Your decimal multiplied by 5 and converted to an integer is: " + newNumFloatInt);
+            Console.ReadLine();
+            //asks user to input number to apply function to and converts a string entered by user to an integer
+            Console.WriteLine("Please input a integer to operate on: ");
+            int newNumDiv = mathFunction.myOverload(Console.ReadLine());
+            // returns message with result of function
+            Console.WriteLine("Your integer divided by 10 is: " + newNumDiv);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodOverloading/MethodOverloading/mathFunction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MethodOverloading
+{   
+    class mathFunction
+    {
+        //this method adds 10 to user's integer input
+        public static int myOverload(int num)
+        {
+            int myNum = num;
+            int myNumAdd = myNum + 10;
+            return myNumAdd;
+        }
+
+        //this method multiplies 5 to user's float/decimal input
+        public static int myOverload(float num)
+        {
+            int myNum = Convert.ToInt32(num);
+            int myNumMult = myNum * 5;
+            return myNumMult;
+        }
+
+        //this method divides user's string input by 10 
+        public static int myOverload(string num)
+        {
+            int myNum = Convert.ToInt32(num);
+            int myNumDiv = myNum / 10;
+            return myNumDiv;
+        }
+    }
+    
+}
