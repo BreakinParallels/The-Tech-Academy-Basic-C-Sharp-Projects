e4f6912b91941a22a1894c7cfe224305a7474c72
BreakinParallels <93020898+BreakinParallels@users.noreply.github.com>
2022-03-10 22:08:29 -0500

    add

--- OperatorsAssignment/OperatorsAssignment/Program.cs
+++ OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -12,6 +12,7 @@ namespace OperatorsAssignment
         {
             Employee person1 = new Employee() { Id = 1 };
             Employee person2 = new Employee() { Id = 2 };
+            //Overload the “==” operator so it checks if two Employee objects are equal by comparing their Id property
             Console.WriteLine(person1 == person2);
             Console.ReadLine();
         }
