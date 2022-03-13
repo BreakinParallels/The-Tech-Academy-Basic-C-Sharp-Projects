60895ec9c10ae98f81a33470539c3547830a8eaf
BreakinParallels <93020898+BreakinParallels@users.noreply.github.com>
2022-03-13 19:56:18 -0400

    add Lambda Assignment
    also edit to TwentyOne game

---
+++ LambdaAssignment/LambdaAssignment/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employees> employees = new List<Employees>();//full employee list
+
+            employees.Add(new Employees { FName = "Joe", LName = "Douglas", Id = 1 });
+            employees.Add(new Employees { FName = "James", LName = "White", Id = 3 });
+            employees.Add(new Employees { FName = "Kevin", LName = "Adams", Id = 1224 });
+            employees.Add(new Employees { FName = "James", LName = "Dean", Id = 1244 });
+            employees.Add(new Employees { FName = "Johnny", LName = "Bravo", Id = 1236 });
+            employees.Add(new Employees { FName = "Joe", LName = "Jordan", Id = 1274 });
+            employees.Add(new Employees { FName = "Anakin", LName = "Skywalker", Id = 2 });
+            employees.Add(new Employees { FName = "Obi Wan", LName = "Kenobi", Id = 1230 });
+            employees.Add(new Employees { FName = "Joe", LName = "Gaines", Id = 1034 });
+            employees.Add(new Employees { FName = "Yasir", LName = "Rocha", Id = 1004 });
+
+            List<Employees> theJoes = new List<Employees>();//new list that all of the joes go into.
+
+            //loop that adds any employee who's first name is joe to a new list
+            foreach (Employees employee in employees)
+            {
+                if (employee.FName == "Joe")
+                    theJoes.Add(employee);
+            }
+
+            //Lambda function to find the first name "Joe" in a list.
+            List<Employees> theJoesFNameLambda = employees.Where(x => x.FName.Contains("Joe")).ToList();
+
+            //Lambda function to find the Id of all employees over 5.
+            List<Employees> theJoesId = employees.Where(x => x.Id > 5).ToList();
+
+            //Code to check for contents of lists
+            //foreach (Employees joe in the theJoesId)
+            //{
+            //    Console.WriteLine(joe.FName + " " + joe.LName + " Id: " + joe.Id);
+            //}
+        }
+
+        class Employees
+        {
+            public string FName { get; set; }
+            public string LName { get; set; }
+            public int Id { get; set; }
+        }
+
+    }
+}
--- TwentyOneGame/TwentyOneGame/Program.cs
+++ TwentyOneGame/TwentyOneGame/Program.cs
@@ -11,14 +11,14 @@ namespace TwentyOneGame
         static void Main(string[] args)
         {
 
-            Deck deck = new Deck();
-            deck.Shuffle(3);
 
-            foreach (Card card in deck.Cards)
-            {
-                Console.WriteLine(card.Face + " of " + card.Suit);
-            }
-            Console.WriteLine(deck.Cards.Count);
+            //deck.Shuffle(3);
+
+            //foreach (Card card in deck.Cards)
+            //{
+            //    Console.WriteLine(card.Face + " of " + card.Suit);
+            //}
+            //Console.WriteLine(deck.Cards.Count);
             Console.ReadLine();
         }
     }
