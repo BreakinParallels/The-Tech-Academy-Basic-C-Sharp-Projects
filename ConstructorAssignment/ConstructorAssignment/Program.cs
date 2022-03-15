7e1897f624a4845f4f1667f18de72f5609d67018
BreakinParallels <93020898+BreakinParallels@users.noreply.github.com>
2022-03-15 17:56:43 -0400

    add ConstructASST

--- ConstructorAssignment/ConstructorAssignment/Program.cs
+++ ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -10,6 +10,7 @@ namespace ConstVarChainConstructors
             const string myName = "Shannon";
             //Created a variable using the keyword “var”
             var primeNumbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Console.ReadLine();
         }
     }
 }
