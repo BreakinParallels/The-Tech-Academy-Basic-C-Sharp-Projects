c40df840a19c7d7adc3d5d822eb8953f5d7af314
BreakinParallels <93020898+BreakinParallels@users.noreply.github.com>
2022-07-15 18:34:42 -0400

    add

---
+++ HelloWorldEX/HelloWorldEX/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+
+
+namespace C_Sharp_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //OPERATORS EX
+
+            //var a = 10;
+            //var b = 3;
+
+            //Console.WriteLine(a + b);
+
+            //var a = 10;
+            //var b = 3;
+
+            //Console.WriteLine(a / b);
+
+            //var a = 10;
+            //var b = 3;
+
+            //Console.WriteLine((float)a / (float)b);
+
+            var a = 1;
+            var b = 2;
+            var c = 3;
+
+            //FOLLOW PENMAS METHOD FOR ARITHMETIC
+
+            //Console.WriteLine(a + b * c);
+            Console.WriteLine((a + b) * c);
+
+
+            //try
+            //{
+
+            //    string str = "true";
+            //    bool b = Convert.ToBoolean(str);
+            //    Console.WriteLine(b);
+
+            //    //var number = "1234";
+            //    //byte b = Convert.ToByte(number);
+            //    //Console.WriteLine(b);
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("The number could not be converted to a byte.");
+
+            //}
+
+            //var number = "1234";
+            //int i = Convert.ToInt32(number);
+            //Console.WriteLine(i);
+
+
+            //int i = 1;
+            //byte b = (byte) i;
+            //Console.WriteLine(b);
+
+
+            //Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            //Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
+            //byte number = 2;
+            //int count = 10;
+            //float totalPrice = 20.95f;
+            //char character ='A';
+            //string firstName = "Mosh";
+            //bool isWorking = false;
+
+            //var number = 2;
+            //var count = 10;
+            //var totalPrice = 20.95f;
+            //var character = 'A';
+            //var firstName = "Mosh";
+            //var isWorking = false;
+
+            //Console.WriteLine(number);
+            //Console.WriteLine(count);
+            //Console.WriteLine(totalPrice);
+            //Console.WriteLine(character);
+            //Console.WriteLine(firstName);
+            //Console.WriteLine(isWorking);
+        }
+    }
+}
---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseYear(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+
+
+
+
+        //{
+        //    var movie = new Movie() { Name = "Shrek" };
+
+        //    return View(movie);
+        //}
+
+        //public ActionResult Edit(int id)
+        //{
+        //    return Content("id=" + id);
+        //}
+        //// movies
+        //public ActionResult Index()
+        //{
+        //    public ActionResult Index(int? pageIndex, string sortBy)
+        //    {
+        //        if (!pageIndex.HasValue)
+        //            pageIndex = 1;
+
+        //        if (String.IsNullOrWhiteSpace(sortBy))
+        //            sortBy = "Name";
+
+        //        return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        //    }
+        //}
+    }
+}
